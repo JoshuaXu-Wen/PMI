183b7af463370090b1d314ab6f191dd4c48f4c3a
JoshuaXu-Wen <66362899+JoshuaXu-Wen@users.noreply.github.com>
2020-10-19 21:32:07 +1300

    Update lens_parallel.cpp

--- ass4/lens_parallel.cpp
+++ ass4/lens_parallel.cpp
@@ -1,6 +1,5 @@
 /* 
    159735 Parallel Programming
-
    Startup program for sequential implementation of simulation by ray
    tracing of gravitational lensing.
  */
@@ -34,27 +33,53 @@ double diffclock(clock_t clock1,clock_t clock2)
   return diffms; // Time difference in milliseconds
 }
 
-__global__ void pixel_add(int* npixx, int* npixy, float* lens_scale, float* rsrc, float* ldc, float* xsrc, float* ysrc)
+
+__device__ void shootParallel(float& xs, float& ys, float xl, float yl, 
+     float* xlens, float* ylens, float* eps, int nlenses)
 {
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
-  
-  //what ix and iy  does this thread work on
-    int ix = index % (*npixx);
-    int iy = index / (*npixy); 
+  float dx, dy, dr;
+  xs = xl;
+  ys = yl;
+  for (int p = 0; p < nlenses; ++p) {
+    dx = xl - xlens[p];
+    dy = yl - ylens[p];
+    dr = dx * dx + dy * dy;
+    xs -= eps[p] * dx / dr;
+    ys -= eps[p] * dy / dr;
+  }
+
+}
+
+
+__global__ void pixel_add(int npixx, int npixy, int lens_scale, int nlenses, float* xlens, float* ylens, float* eps, float *lensim)
+{
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  const float rsrc = 0.1;      // radius
+  const float ldc  = 0.5;      // limb darkening coefficient
+  const float xsrc = 0.0;      // x and y centre on the map
+  const float ysrc = 0.0;
+  const float rsrc2 = rsrc * rsrc;
+  if (index <= npixx * npixy) {
+    //what ix and iy  does this thread work on
+    int ix = index % npixx;
+    int iy = index / npixy; 
     float xl, yl, xs, ys, sep2, mu;
-    float xd, yd;
-  //int i = blockDim.x * blockIdx.x + threadIdx.x;
+    float xd, yd, dx, dy, dr;
+
     yl = YL1 + iy * lens_scale;
     xl = XL1 + ix * lens_scale;
-    shoot(xs, ys, xl, yl, xlens, ylens, eps, nlenses);
-    xd = xs - *xsrc;
-    yd = ys - *ysrc;
+    shootParallel(xs, ys, xl, yl, xlens, ylens, eps, nlenses);
+    xd = xs - xsrc;
+    yd = ys - ysrc;
     sep2 = xd * xd + yd * yd;
     if (sep2 < rsrc2) {
-        mu = sqrt(1 - sep2 / (*rsrc2));
-	lensim(iy, ix) = 1.0 - (*ldc * (1 - mu);
+      mu = sqrt(1 - sep2 / rsrc2);
+      lensim[index] = 1.0 - (ldc * (1 - mu));
     }
+  }
+  
 }
+    
 
 int main(int argc, char* argv[]) 
 {
@@ -66,59 +91,41 @@ int main(int argc, char* argv[])
   float* eps;
   const int nlenses = set_example_1(&xlens, &ylens, &eps);
   std::cout << "# Simulating " << nlenses << " lens system" << std::endl;
+  size_t size = nlenses * sizeof(float);
+ 
+ // std::cout << "# Building " << npixx << "X" << npixy << " lens image" << std::endl;
 
-  // Source star parameters. You can adjust these if you like - it is
-  // interesting to look at the different lens images that result
-  const float rsrc = 0.1;      // radius
-  const float ldc  = 0.5;      // limb darkening coefficient
-  const float xsrc = 0.0;      // x and y centre on the map
-  const float ysrc = 0.0;
-// Pixel size in physical units of the lens image. You can try finer
-// lens scale which will result in larger images (and take more
-// time).
    const float lens_scale = 0.005;
 // Size of the lens image
    const int npixx = static_cast<int>(floor((XL2 - XL1) / lens_scale)) + 1;
    const int npixy = static_cast<int>(floor((YL2 - YL1) / lens_scale)) + 1;
- 
-
- 
-  std::cout << "# Building " << npixx << "X" << npixy << " lens image" << std::endl;
 
   // Put the lens image in this array
   Array<float, 2> lensim(npixy, npixx);
+  size_t size_lensim = (npixy*npixy) * sizeof(float)
+
+  float *dev_xlens, *dev_ylens, *dev_eps, *dev_lensim,
+  cudaMalloc(&dev_xlens, size);
+  cudaMalloc(&dev_ylens, size);
+  cudaMalloc(&dev_eps, size);
+  cudaMalloc(&dev_lensim, size_lensim);
+
+  cudaMemcpy(dev_xlens, xlens, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_ylens, ylens, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_eps, eps, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_lensim, lensim.buffer, size_lensim, cudaMemcpyHostToDevice);
+
 
   clock_t tstart = clock();
-
-  // Draw the lensing image map here. For each pixel, shoot a ray back
-  // to the source plane, then test whether or or not it hits the
-  // source star
-  const float rsrc2 = rsrc * rsrc;
+  pixel_add<<< N/THREADS_PER_BLOCK, THREADS_PER_BLOCK >>>(npixx, npixy, lens_scale, nlenses, dev_xlens, dev_ylens, dev_eps, dev_lensim);
  
   int numuse = 0;
 
-  //for (int iy = 0; iy < npixy; ++iy) 
-  //    for (int ix = 0; ix < npixx; ++ix) { 
 
-    // YOU NEED TO COMPLETE THIS SECTION OF CODE
+  // Copy result from device memory into host memory
+  cudaMemcpy(lensim.buffer, dev_lensim, size, cudaMemcpyDeviceToHost);
 
-    // need position on lens in physical units
 
-    // shoot a ray back to the source plane - make the appropriate
-    // call to shoot() in lenses.h
-
-    // does the ray hit the source star?
-/*	yl = YL1 + iy * lens_scale;
-	xl = XL1 + ix * lens_scale;
-	shoot(xs, ys, xl, yl, xlens, ylens, eps, nlenses);
-	xd = xs - xsrc;
-	yd = ys - ysrc;
-	sep2 = xd * xd + yd * yd;
-	if (sep2 < rsrc2) {
-	mu = sqrt(1 - sep2 / rsrc2);
-	lensim(iy, ix) = 1.0 - ldc * (1 - mu);
-	}
-  }*/
 
   clock_t tend = clock();
   double tms = diffclock(tend, tstart);
